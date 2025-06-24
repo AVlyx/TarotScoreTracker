e30e67a0ce2cba643b05b3d5f1ec815f43a48217
AVlyx <anton@vancleem.com>
2025-06-23 21:37:57 -0400

    rm pyinstaller

--- run_app.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-# Replace the path below with the full path to your Python executable
-python_path = r"C:\Users\anton\Desktop\projects\TarotScoreTracker\.venv\Scripts\python.exe"
-subprocess.run([python_path, "-m", "streamlit", "run", "app.py"])
